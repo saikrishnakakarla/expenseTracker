baf68e4da2167a2680e84f4410782fac6ae87cd1
saikrishnakakarla <50262791+saikrishnakakarla@users.noreply.github.com>
2025-12-31 21:04:33 +0530

    Update ExpensetrackerApplication.java

--- expensetracker/src/main/java/com/artha/expensetracker/ExpensetrackerApplication.java
+++ expensetracker/src/main/java/com/artha/expensetracker/ExpensetrackerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ExpensetrackerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("DB URL: " + System.getenv("DB_HOST") + ":" + System.getenv("DB_PORT") + "/" + System.getenv("DB_NAME"));
 		SpringApplication.run(ExpensetrackerApplication.class, args);
 	}
 
